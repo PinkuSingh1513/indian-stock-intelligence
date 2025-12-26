86b2b5d88fc884811da86a57ef461a705c338986
PinkuSingh1513 <pinkupanwar01@yahoo.co.in>
2025-12-27 03:16:13 +0530

    Create macro.js

---
+++ js/macro.js
@@ -0,0 +1,5 @@
+document.getElementById("macro").innerHTML = `
+<p><b>USD-INR:</b> Impacts IT & Pharma</p>
+<p><b>FII Flow:</b> Large caps bullish when positive</p>
+<p><b>Crude Oil:</b> Impacts Airlines & Paints</p>
+`;
